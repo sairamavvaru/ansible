80f19fe485d272d8da81e810931408920f2d1344
avvarusairam <avvarusairam123@gmail.com>
2022-01-24 12:44:49 +0530

    variables task level variables

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -3,4 +3,4 @@
 #2. Take that Instance IP & register in DNS
 
 aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specification
- "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]"
+ "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" | jq
