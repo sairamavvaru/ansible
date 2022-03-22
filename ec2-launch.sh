32d83916ec738f2780c036b142e28d13c769fadf
avvarusairam <avvarusairam123@gmail.com>
2022-03-22 20:15:27 +0530

    variables task level variable

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -6,5 +6,5 @@
 TEMP_ID="lt-0433da4e92eb56542"
 TEMP_VER=1
 
-aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Verstion=${TEMP_VER}
+aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Verstion=${TEMP_VER} | jq
 
