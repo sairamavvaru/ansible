f65e43e1936a18025aa7c313d77b1eaa805ea690
avvarusairam <avvarusairam123@gmail.com>
2022-03-22 20:05:27 +0530

    variables task level variable

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -2,4 +2,9 @@
 #1. Create an instance
 #2. Take that Instance IP & register in DNS
 
-aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specification "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" | jq
+
+TEMP_ID="lt-0433da4e92eb56542"
+VER_ID=1
+
+aws ec2 run-instances --linch-template LunchTemplateId=${TEMP_ID},Verstion=${VAR_VER}
+
