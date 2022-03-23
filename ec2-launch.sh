1ea116ae86df5b927014566c4358eca1ceac67a6
avvarusairam <avvarusairam123@gmail.com>
2022-03-23 11:51:56 +0530

    variables task level variable

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -7,6 +7,11 @@ TEMP_ID="lt-0433da4e92eb56542"
 TEMP_VER=2
 
 ## Check if instance is already there
+aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" | jq .Reservations[].Instances[].State.Name | jq | sed  's/"//g' | grep -E 'running|stopped' &>/dev/null
+if [ $? -eq -0 ]; then
+  echo "instance is already there"
+  exit
+fi
 
 aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{key=Name,Value=frontend}]" "ResourceType=instance,Tages={[Key=Name,Value=frontend}]" | jq
 
