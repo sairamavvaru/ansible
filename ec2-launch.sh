1942dba86ec0ed15781a648db2e5c7b31a910e67
avvarusairam <avvarusairam123@gmail.com>
2022-03-23 12:02:39 +0530

    variables task level variable

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -2,12 +2,19 @@
 #1. Create an instance
 #2. Take that Instance IP & register in DNS
 
+if [ -z "$1" ]; then
+  echo "input is missing"
+  exit 1
+fi
+
+COMPONENT=$1
+
 
 TEMP_ID="lt-0433da4e92eb56542"
 TEMP_VER=2
 
 ## Check if instance is already there
-aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" | jq .Reservations[].Instances[].State.Name | jq | sed  's/"//g' | grep -E 'running|stopped' &>/dev/null
+aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | jq | sed  's/"//g' | grep -E 'running|stopped' &>/dev/null
 if [ $? -eq -0 ]; then
   echo "instance is already there"
   exit
