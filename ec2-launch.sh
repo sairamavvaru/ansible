7b33b71be55b7acd1ddc38c2bfbc564980a33ac7
avvarusairam <avvarusairam123@gmail.com>
2022-03-25 20:19:53 +0530

    variables task level variable

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -24,7 +24,7 @@ fi
 
 aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{key=Name,Value=${COMPONENT}}]" "ResourceType=instance,Tages=[{Key=Name,Value=${COMPONENT}}]" | jq
 
-IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" | jq .Reservations[].Instances[].PrivateIpAddress | sed 's/"//g')
+IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" | jq .Reservations[].Instances[].PrivateIpAddress | sed 's/"//g') | grep -v null)
 
 # update the DNS record
 sed -e "s/IPADDRESS/${IPADDRESS}/" -e "s/COMPONENT/${COMPONENT}/" record.json >/tmp/record.json
