4f1bba729788807a9f4a21375e017d36a4690cde
avvarusairam <avvarusairam123@gmail.com>
2022-03-25 18:54:40 +0530

    variables task level variable

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -16,7 +16,7 @@ ZONE_ID=Z0996673224U8ZV493LEV
 
 
 ## Check if instance is already there
-
+CREATE_INSTSNCE(){
 aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | sed  's/"//g' | grep -E 'running|stopped' &>/dev/null
 if [ $? -eq -0 ]; then
   echo -e  "\e[1;33mInstance is already there\e[0m"
@@ -25,11 +25,15 @@ fi
 
 aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{key=Name,Value=${COMPONENT}}]" "ResourceType=instance,Tages=[{Key=Name,Value=${COMPONENT}}]" | jq
 
-IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" | jq .Reservations[].Instances[].PrivateIpAddress | sed 's/"//g')
+IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" | jq .Reservations[].Instances[].PrivateIpAddress | sed 's/"//g') | grep -v null
 
 # update the DNS record
 sed -e "s/IPADDRESS/${IPADDRESS}/" -e "s/COMPONENT/${COMPONENT}/" record.json >/tmp/record.json
 aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
+}
+if [ "$COMPONENT" "" 'all']; then
+  for comp in frontend mongodb catalogue ; do
+
 
 
 
